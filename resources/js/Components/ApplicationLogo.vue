fff99b49de6c88af182ce190562c3b1d735ca7f4
Caio Alves <kaio_rx@hotmail.com>
2023-03-14 14:56:27 -0300

    first commit

---
+++ resources/js/Components/ApplicationLogo.vue
@@ -0,0 +1,3 @@
+<template>
+   <img src="../../../public/assets/Pharmaco_logo.svg" alt="">
+</template>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <div
+        class=""
+    >
+        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+            <Link
+                v-if="$page.props.auth.user"
+                :href="route('dashboard')"
+                class=""
+                >Dashboard</Link
+            >
+
+            <template v-else>
+
+
+
+                <Link
+                    :href="route('login')"
+                    class=""
+                    >Entrar</Link
+                >
+
+                <Link
+                    v-if="canRegister"
+                    :href="route('register')"
+                    class="ml-4 "
+                    >Cadastrar</Link
+                >
+            </template>
+        </div>
+
+
+    </div>
+</template>
+
+<style>
+
+</style>
